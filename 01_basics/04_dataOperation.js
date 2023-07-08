0dccd94c58f56b26990fcf490a8231f86c5b01e4
fullstop <87227215+full-stop-off@users.noreply.github.com>
2023-07-08 03:57:31 +0000

    Data Operations

---
+++ 01_basics/04_dataOperation.js
@@ -0,0 +1,26 @@
+// Strings | Numbers Operations
+
+let value = 5;
+let negativeValue = -value;
+console.log(negativeValue); // -5
+
+// Arithmetic Operations
+console.log(2 + 2); //4
+console.log(2 - 2); // 0
+console.log(2 * 2); // 4
+console.log(8 / 2); // 4
+console.log(4 ** 2); // 16
+console.log(11 % 3); // 2 - Useful on Cyptography, Encryption algorithms and soon
+
+let fname = "full";
+let lname = " stop";
+
+console.log(fname + lname); // full stop
+console.log(fname + 1); // full1
+console.log(1 + lname); // 1 stop
+
+// JS computes from LEFT to RIGT
+console.log(1 + "2"); // 12
+console.log("1" + 2); // 12
+console.log(1 + 2 + "3"); // 33
+console.log("1" + 2 + 3); // 123
